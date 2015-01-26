35331f46f18232770b9083a4c07963362fc8b011
Ricardo Quesada <ricardoquesada@gmail.com>
2015-01-25 21:25:44 -0800

    adds License header

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,5 +1,20 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+/****************************************************************************
+Copyright 2015 Ricardo Quesada
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+****************************************************************************/
+
+#pragma once
 
 #include <QMainWindow>
 #include <QString>
@@ -36,4 +51,3 @@ private:
     QString _lastDir;
 };
 
-#endif // MAINWINDOW_H
--- src/state.h
+++ src/state.h
@@ -1,5 +1,20 @@
-#ifndef STATE_H
-#define STATE_H
+/****************************************************************************
+Copyright 2015 Ricardo Quesada
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+****************************************************************************/
+
+#pragma once
 
 #include <QString>
 #include <string>
@@ -72,4 +87,3 @@ protected:
     int _colors[4];
 };
 
-#endif // STATE_H
