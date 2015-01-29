3ad8c1ce8152fbfc2d1c4628bae1a4bbc76b13be
Ricardo Quesada <ricardoquesada@gmail.com>
2015-01-29 10:50:24 -0800

    icons, c++11 R for about

--- src/aboutdialog.cpp
+++ src/aboutdialog.cpp
@@ -17,18 +17,23 @@ limitations under the License.
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
 
+
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
 
+    const char *htmlraw = R"html(
+<p align="center"><font size="+2"><b>VChar64</b></font><br><i>Version %1</i></p>
+<p align="center">Copyright 2015 Ricardo Quesada<br>(see the <a href="https://github.com/ricardoquesada/vchar64/blob/master/AUTHORS">AUTHORS</a> file for a full list of contributors)</p>
+<p align="center">You may modify and redistribute this program under the terms of the <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License v2.0.<a/>
+<p align="center"><a href="https://github.com/ricardoquesada/vchar64/">https://github.com/ricardoquesada/vchar64</a></p>
+)html";
+
     const QString html = QCoreApplication::translate(
             "AboutDialog",
-            "<p align=\"center\"><font size=\"+2\"><b>VChar64</b></font><br><i>Version %1</i></p>\n"
-            "<p align=\"center\">Copyright 2015 Ricardo Quesada<br>(see the AUTHORS file for a full list of contributors)</p>\n"
-            "<p align=\"center\">You may modify and redistribute this program under the terms of the <a href=\"http://www.apache.org/licenses/LICENSE-2.0\">Apache License v2.0.<a/>"
-            "<p align=\"center\"><a href=\"http://towp8.com/\">http://towp8.com/</a></p>\n")
+                htmlraw)
             .arg(QApplication::applicationVersion());
 
     ui->textBrowser->setHtml(html);
