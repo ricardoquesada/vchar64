a5d90dd365011f5a2e63ba8f8513bdabe388c87a
Ricardo Quesada <ricardoquesada@gmail.com>
2015-08-24 21:33:33 -0700

    better defaults for windows

--- src/bigchar.cpp
+++ src/bigchar.cpp
@@ -166,7 +166,7 @@ void BigChar::paintEvent(QPaintEvent *event)
     painter.begin(this);
 
     // background
-    painter.fillRect(event->rect(), QColor(204,204,204));
+    painter.fillRect(event->rect(), QColor(240,240,240));
 
     QPen pen;
     pen.setColor({149,195,244,255});
